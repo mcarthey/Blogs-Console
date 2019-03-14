02c70a2dacaae00ce21401fb59ed8191d40070bb
jgrissom <jgrissom91@gmail.com>
2019-03-13 21:30:47 -0500

    Program.cs
    1) Input blog name
    2) Add blog to database
    3) Display all blogs in database (use LINQ to sort blogs by name)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using NLog;
+using BlogsConsole.Models;
+using System;
+using System.Linq;
 
 namespace BlogsConsole
 {
     class MainClass
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            logger.Info("Program started");
+            try
+            {
+
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+
+                var db = new BloggingContext();
+                db.AddBlog(blog);
+                logger.Info("Blog added - {name}", name);
+
+                // Display all Blogs from the database
+                var query = db.Blogs.OrderBy(b => b.Name);
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            logger.Info("Program ended");
         }
     }
 }
