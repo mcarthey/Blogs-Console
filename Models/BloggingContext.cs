97830d2d5fbd0998ad4f5239815924f616031fe4
jgrissom <jgrissom91@gmail.com>
2019-03-13 21:25:59 -0500

    Data Model
    1) Add Models folder
    2) Add Blog class
    3) Add Post class
    4) Add BloggingContext class

---
+++ Models/BloggingContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace BlogsConsole.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext() : base("name=BlogContext") { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public void AddBlog(Blog blog)
+        {
+            this.Blogs.Add(blog);
+            this.SaveChanges();
+        }
+    }
+}
